9a4f74fb6d29abda8cf3a62aa9a2b066fc3db3d0
DiabeticSam <sammarriott5@hotmail.com>
2014-11-07 15:00:49 +0000

    update1

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
+
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
+}
+
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+	var key = “aStringForKey”; 
+	var value = “aStringForValue”; 
+	
+	window.localStorage.setItem( key, value );
+}
